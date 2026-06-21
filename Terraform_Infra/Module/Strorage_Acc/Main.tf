b343e7c647bdc3e292f5dbdf8918b6b4614e04c9
Yogesh <57596415+yogesh235@users.noreply.github.com>
2026-06-21 08:57:19 +0530

    Refactor storage account and container configurations for AKS infrastructure; remove recovery service variable and update variable definitions.

--- Terraform_Infra/Module/Strorage_Acc/Main.tf
+++ Terraform_Infra/Module/Strorage_Acc/Main.tf
@@ -1,27 +1,19 @@
-data "azurerm_resource_group" "RG_Dev" {
-  name = var.RG_Info
+data "azurerm_resource_group" "RG_Info" {
+  name     = var.RG_Info
 }
 
-resource "azurerm_recovery_services_vault" "vault" {
-  for_each            = var.Recovery_Service
-  name                = each.value.name
-  location            = data.azurerm_resource_group.RG_Dev.location
-  resource_group_name = data.azurerm_resource_group.RG_Dev.name
-  sku                 = each.value.sku
-}
-
-resource "azurerm_storage_account" "Stroage" {
-  for_each                 = var.storage_Info
+resource "azurerm_storage_account" "Stg_Info" {
+  for_each = var.storage_Info
   name                     = each.value.name
-  location                 = data.azurerm_resource_group.RG_Dev.location
-  resource_group_name      = data.azurerm_resource_group.RG_Dev.name
+  resource_group_name      = data.azurerm_resource_group.RG_Info.name
+  location                 = data.azurerm_resource_group.RG_Info.location
   account_tier             = each.value.account_tier
   account_replication_type = each.value.account_replication_type
 }
 
-resource "azurerm_backup_container_storage_account" "container" {
-  for_each            = var.container
-  resource_group_name = data.azurerm_resource_group.RG_Dev.name
-  recovery_vault_name = azurerm_recovery_services_vault.vault[each.value.Recovery_Var].name
-  storage_account_id  = azurerm_storage_account.Stroage[each.value.Storage_Var].id
+resource "azurerm_storage_container" "container_Info" {
+  for_each = var.container
+  name                  = each.value.name
+  storage_account_name  = azurerm_storage_account.Stg_Info[each.value.storage_account_name].name
+  container_access_type = each.value.container_access_type
 }
--- Terraform_Infra/Root/Storage_Acc/Main.tf
+++ Terraform_Infra/Root/Storage_Acc/Main.tf
@@ -1,7 +1,6 @@
 module "Storage_Information" {
   source = "../../Module/Strorage_Acc"
   RG_Info=var.RG_Info
-  Recovery_Service= var.Recovery_Service
   storage_Info= var.storage_Info
   container= var.container
 }
